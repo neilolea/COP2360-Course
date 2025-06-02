4ed68f8746e9f7e46891d74b605799a1f41b231d
Neil Olea <69611050+neilolea@users.noreply.github.com>
2025-06-01 23:14:37 -0400

    Add files via upload

---
+++ Octopi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Octopus
+{
+    public string name;
+    public int Age = 2;
+    public string Color = "purple";
+    static readonly int legs = 8;
+
+    public void Swim()
+    {
+        Console.WriteLine($"{name} the {Color} octopus is now swimming");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Octopus[] myOctopuses = new Octopus[3];
+
+        myOctopuses[0] = new Octopus();
+        myOctopuses[0].name = "Steven";
+        myOctopuses[0].Color = "red";
+
+        myOctopuses[1] = new Octopus();
+        myOctopuses[1].name = "Ocean";
+        myOctopuses[1].Age = 5;
+
+        myOctopuses[2] = new Octopus();
+        myOctopuses[2].name = "Todd";
+        myOctopuses[2].Color = "blue";
+
+        for (int i = 0; i < myOctopuses.Length; i++)
+        {
+            Console.WriteLine($"{myOctopuses[i].name} is {myOctopuses[i].Age} years old");
+            myOctopuses[i].Swim();
+        }
+
+        Console.ReadKey();
+    }
+}
